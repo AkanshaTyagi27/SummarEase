60f05cd12d8ddecb1347881afcbe646e0fa33467
AkanshaTyagi27 <akanshatyagi2003@gmail.com>
2024-04-25 18:51:11 +0530

    data validation completed

---
+++ src/SummarEase/pipeline/stage_02_data_validation.py
@@ -0,0 +1,15 @@
+
+from SummarEase.config.configuration import ConfigurationManager
+from SummarEase.components.data_validation import DataValidation
+from SummarEase.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+        
