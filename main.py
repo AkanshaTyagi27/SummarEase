c97daa021222d5dd2a3270d2afb8886d1162bfdc
AkanshaTyagi27 <akanshatyagi2003@gmail.com>
2024-04-25 20:45:21 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from SummarEase.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from SummarEase.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from SummarEase.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from SummarEase.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,5 +25,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
